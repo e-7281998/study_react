04c7091354b25bc06f5810d0313b75cae1bf7ea6
e-7281998 <jejjcs43@naver.com>
2021-11-11 15:15:55 +0900

    추가 기능

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './scss/TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+
+  const [ value, setValue ] = useState ('');
+
+  const onChange = useCallback( e => {
+    setValue(e.target.value);
+  },[]);  //두번 째 배열에 조건이 없으므로 처음 렌더링 될 때 생성
+
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue('');
+
+      //submit 이벤트는 브라우저에서 새로고침을 발생시킴, 이를 방지
+      e.preventDefault();
+    },
+    [onInsert, value]
+  )
   return (
-      <form className="TodoInsert">
-        <input placeholder="할 일을 입력하세요"/>
+      <form className="TodoInsert" onSubmit={onSubmit}>
+        <input
+          placeholder="할 일을 입력하세요"
+          value={value}
+          onChange={onChange}
+        />
         <button type="submit">
           <MdAdd />
         </button>
