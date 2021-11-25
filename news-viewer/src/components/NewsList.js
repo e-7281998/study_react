c81f2a362c2a57b8597c64c6ea56d11050c044fb
e-7281998 <jejjcs43@naver.com>
2021-11-25 17:12:20 +0900

    [Categories.js],[NewsList.js] 카테고리 기능

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -16,8 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
-
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -25,8 +24,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=468cb33fd8ca4901849a56b088a72094',
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=468cb33fd8ca4901849a56b088a72094`,
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -35,7 +35,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   //대기 중
   if(loading) {
