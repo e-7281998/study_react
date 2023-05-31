188da0d842bdb2192623a031d4d6cfe9520b342a
e-7281998 <jejjcs43@naver.com>
2023-05-31 12:33:17 +0900

    내 위치 가져오기 Geolocation

--- map/src/components/Map.js
+++ map/src/components/Map.js
@@ -1,19 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/map.css";
 
 const { kakao } = window;
 
 function Map(props) {
+  const [latitude, setLatitude] = useState(33.450701);
+  const [longitude, setLongitude] = useState(126.570667);
+
   useEffect(() => {
+    //현 위치 가져오기
+    if (navigator.geolocation) {
+      //Geolocation 이용해 접속 위치 얻어오기
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          //console.log("위치 가져오기 OK");
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+
     const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
     const options = {
       //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      center: new kakao.maps.LatLng(latitude, longitude), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
 
     const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-  }, []);
+  }, [latitude, longitude]);
 
   return <div id="map"></div>;
 }
