0e9cbc66007cd39dc607dabca7df960512230dbe
e-7281998 <jejjcs43@gmail.com>
2023-06-08 17:12:03 +0900

    [마커이미지변경]

--- map/src/components/Map.js
+++ map/src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../css/map.css";
+import marker_A from "../img/marker_A.png";
 
 const { kakao } = window;
 
@@ -32,11 +33,23 @@ function Map(props) {
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
 
+    // ** 마커 이미지 변경
+    const imageSrc = marker_A, // 마커이미지의 주소입니다
+      imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+      imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+    const markerImage = new kakao.maps.MarkerImage(
+      imageSrc,
+      imageSize,
+      imageOption
+    );
+    // **
     const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
       position: markerPosition,
+      image: markerImage, //마커이미지 추가
     });
 
     // 마커가 지도 위에 표시되도록 설정합니다
